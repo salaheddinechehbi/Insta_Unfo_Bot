3b7cc085f8df1992d4f46bb884689de394538483
salaheddinechehbi <chehbi.se@gmail.com>
2020-02-17 01:40:59 +0100

    sleep function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
-
+from time import sleep
 
 class InstaBot:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://instagram.com")
+        sleep(10)
     
 
+#Twitter()
 InstaBot()
