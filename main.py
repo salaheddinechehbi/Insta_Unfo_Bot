ce162d5177ec8349b951d57a1e7b8a46c0594f1c
salaheddinechehbi <chehbi.se@gmail.com>
2020-02-22 12:54:10 +0100

    Send username and password to the inputs

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ class InstaBot:
     def __init__(self,username,pw):
         self.driver = webdriver.Chrome()
         self.driver.get("https://instagram.com")
-        sleep(10)
+        sleep(5)
         self.driver.find_element_by_xpath("//a[contains(text(), 'Connectez-vous')]").click()
-        sleep(10)
+        sleep(5)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+        sleep(5)
     
 
-#Twitter()
 fh = open("compte.txt","r")
-my_text_pw = str(fh.readline())
 my_text_username = str(fh.readline())
-"""print(my_text_pw)
-print(my_text_username)"""
+my_text_pw = str(fh.readline())
+#print(my_text_pw)
+#print(my_text_username)
 InstaBot(my_text_username,my_text_pw)
