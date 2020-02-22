901f4a9785efee6ad40a8f0b3d38da12b665bc88
salaheddinechehbi <chehbi.se@gmail.com>
2020-02-22 12:03:23 +0100

    add text file with username and password

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from time import sleep
 
 class InstaBot:
 
-    def __init__(self):
+    def __init__(self,username,pw):
         self.driver = webdriver.Chrome()
         self.driver.get("https://instagram.com")
         sleep(10)
     
 
 #Twitter()
-InstaBot()
+fh = open("compte.txt","r")
+
+my_text_pw = str(fh.readline())
+my_text_username = str(fh.readline())
+print(my_text_pw)
+print(my_text_username)
+InstaBot(my_text_username,my_text_pw)
